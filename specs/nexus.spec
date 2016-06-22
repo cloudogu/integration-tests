4bcd2fe5b63b57d939fa7997e9a77d234fba1d68
Maltek709 <maltekreutzfeldt@web.de>
2016-06-22 15:21:32 +0200

    extended nexus.spec with new scenario for group testing. Inserted new steps into NexusSteps

--- specs/nexus.spec
+++ specs/nexus.spec
@@ -43,3 +43,18 @@ Log in to Nexus and test Single Sign out via cas/logout
 
 * Nexus-Login "admin" with password "password" for Single Sign out
 * Log out from Jenkins via cas/logout
+
+
+Groups
+------
+Tags: nexus_groups
+
+Access Nexus-API as user in group with admin rights, test if administration
+privileges are given and test opposite with user not in group with admin rights
+
+* Access Nexus API as "admin" with password "password" with admin rights
+* Check if access to Nexus file accepted
+* Quit Nexus client with admin rights
+* Access Nexus API as "dummyuser" with password "dummyuser123" without admin rights
+* Check if access to Nexus file not accepted
+* Quit Nexus client without admin rights
--- src/test/java/com/cloudogu/ces/NexusSteps.java
+++ src/test/java/com/cloudogu/ces/NexusSteps.java
@@ -7,8 +7,11 @@ package com.cloudogu.ces;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.thoughtworks.gauge.Step;
+import com.thoughtworks.gauge.datastore.DataStore;
+import com.thoughtworks.gauge.datastore.DataStoreFactory;
 import driver.Driver;
 import java.util.Iterator;
+import javax.ws.rs.ForbiddenException;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
@@ -81,5 +84,53 @@ public class NexusSteps {
         // be sure we are redirected to cas
         openNexus();
         assertThat(Driver.webDriver.getTitle(), startsWith("CAS"));
+    }
+    /*-----------------------------------
+    Szenario 5
+    -----------------------------------*/
+    @Step("Access Nexus API as <user> with password <password> with admin rights")
+    public void accessRestApiAsAdmin(String user, String password){
+        NexusAPI api = new NexusAPI(user,password);
+        DataStore scenarioStore = DataStoreFactory.getScenarioDataStore();
+        scenarioStore.put("user", user);
+        scenarioStore.put("nexus_api_admin",api);
+    }
+    @Step("Check if access to Nexus file accepted")
+    public void acceptAccessAsAdmin(){
+        DataStore scenarioStore = DataStoreFactory.getScenarioDataStore();
+        String user = (String) scenarioStore.get("user");
+        NexusAPI api = (NexusAPI) scenarioStore.get("nexus_api_admin");
+        JsonNode jnode = api.getInformation();
+        Iterator<JsonNode> elements = jnode.elements();       
+        String url = elements.next().get(0).get("resourceURI").asText();
+        assertThat(url, is(EcoSystem.getUrl("/nexus/service/local/users/"+user)));
+    }
+    @Step("Quit Nexus client with admin rights")
+    public void quitAdminClient(){
+        DataStore scenarioStore = DataStoreFactory.getScenarioDataStore();
+        NexusAPI api = (NexusAPI) scenarioStore.get("nexus_api_admin");
+        api.close();
+    }
+    @Step("Access Nexus API as <user> with password <password> without admin rights")
+    public void accessRestApiNotAsAdmin(String user, String password){
+        NexusAPI api = new NexusAPI(user,password);
+        DataStore scenarioStore = DataStoreFactory.getScenarioDataStore();
+        scenarioStore.put("nexus_api_noadmin",api);
+    }
+    @Step("Check if access to Nexus file not accepted")
+    public void acceptAccessNotAsAdmin(){
+        DataStore scenarioStore = DataStoreFactory.getScenarioDataStore();
+        NexusAPI api = (NexusAPI) scenarioStore.get("nexus_api_noadmin");
+        try{
+            JsonNode n = api.getInformation();
+        }catch(ForbiddenException e){
+            //System.out.println(e);
+        }
+    }
+    @Step("Quit Nexus client without admin rights")
+    public void quitNonAdminClient(){
+        DataStore scenarioStore = DataStoreFactory.getScenarioDataStore();
+        NexusAPI api = (NexusAPI) scenarioStore.get("nexus_api_noadmin");
+        api.close();
     }
 }
