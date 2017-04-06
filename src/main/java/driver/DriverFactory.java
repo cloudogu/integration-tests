9397ff4ab1389722cbb51f8725bb853195d491a0
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2017-04-06 11:41:18 +0200

    added support for remote selenium tests

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,9 +1,14 @@
 package driver;
 
+import com.google.common.base.Preconditions;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DriverFactory {
 
@@ -11,13 +16,15 @@ public class DriverFactory {
     // There are various implementations for this depending on browser. The required browser can be set as an environment variable.
     // Refer http://getgauge.io/documentation/user/current/managing_environments/README.html
     public static WebDriver getDriver() {
-
         String browser = System.getenv("BROWSER");
+        
         if (browser == null) {
             return new FirefoxDriver();
         }
         switch (browser)
         {
+          case "REMOTE":
+                return createRemoteWebDriver();
             case "IE":
                 return new InternetExplorerDriver();
             case "CHROME":
@@ -25,5 +32,15 @@ public class DriverFactory {
             default:
                 return new FirefoxDriver();
         }
+    }
+    
+    private static RemoteWebDriver createRemoteWebDriver() {
+      String seleniumUrl = System.getenv("SELENIUM_URL");
+      Preconditions.checkNotNull(seleniumUrl, "SELENIUM_URL environment variable not found");
+      try {
+        return new RemoteWebDriver(new URL(seleniumUrl), DesiredCapabilities.chrome());
+      } catch (MalformedURLException ex) {
+        throw new IllegalStateException("found malformed selenium url, please check SELENIUM_URL environment variable", ex);
+      }
     }
 }
