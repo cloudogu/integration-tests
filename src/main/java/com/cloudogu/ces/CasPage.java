59e23f7dbe95e926a70034b999b50e0ed67e1b67
Oliver Milke <oliver.milke@cloudogu.com>
2017-04-20 09:22:50 +0200

    Waiting for Page Reload after Login

--- src/main/java/com/cloudogu/ces/CasPage.java
+++ src/main/java/com/cloudogu/ces/CasPage.java
@@ -5,14 +5,21 @@
  */
 package com.cloudogu.ces;
 
+import com.thoughtworks.gauge.Gauge;
+import driver.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  *
  * @author malte
  */
 public class CasPage {
+
+    @FindBy(tagName = "html")
+    private WebElement htmlElement;
     
     @FindBy(id = "username")
     private WebElement usernameInput;
@@ -26,7 +33,13 @@ public class CasPage {
     public void login(String username, String password){
         usernameInput.sendKeys(username);
         passwordInput.sendKeys(password);
+
         submitButton.click();
+
+        long startingTime = System.currentTimeMillis();
+        new WebDriverWait(Driver.webDriver, 10).until(ExpectedConditions.stalenessOf(htmlElement));
+
+        Gauge.writeMessage(String.format("waited %,dms for login", System.currentTimeMillis() - startingTime) );
     }
 
     
