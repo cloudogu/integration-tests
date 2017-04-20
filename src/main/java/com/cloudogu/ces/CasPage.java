5bfc1c078143a54a35c7a7b2dfd0c060b8f832b8
Oliver Milke <oliver.milke@cloudogu.com>
2017-04-20 11:59:13 +0200

    Removed ignoring of timeouts

--- src/main/java/com/cloudogu/ces/CasPage.java
+++ src/main/java/com/cloudogu/ces/CasPage.java
@@ -8,7 +8,6 @@ package com.cloudogu.ces;
 import com.thoughtworks.gauge.Gauge;
 import driver.Driver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -41,7 +40,6 @@ public class CasPage {
 
         long startingTime = System.currentTimeMillis();
         new WebDriverWait(Driver.webDriver, 10)
-                .ignoring(TimeoutException.class)
                 .until(ExpectedConditions.stalenessOf(htmlElement));
 
         Gauge.writeMessage(String.format("waited %,dms for login", System.currentTimeMillis() - startingTime));
