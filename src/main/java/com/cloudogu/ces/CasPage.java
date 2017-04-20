22747f436637d798c7f18164d73b2c068e06121e
Oliver Milke <oliver.milke@cloudogu.com>
2017-04-20 11:05:48 +0200

    improved waiting for page reload

--- src/main/java/com/cloudogu/ces/CasPage.java
+++ src/main/java/com/cloudogu/ces/CasPage.java
@@ -7,40 +7,45 @@ package com.cloudogu.ces;
 
 import com.thoughtworks.gauge.Gauge;
 import driver.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
- *
  * @author malte
  */
 public class CasPage {
 
-    @FindBy(tagName = "html")
-    private WebElement htmlElement;
-    
     @FindBy(id = "username")
     private WebElement usernameInput;
-    
+
     @FindBy(id = "password")
     private WebElement passwordInput;
-    
+
     @FindBy(name = "submit")
     private WebElement submitButton;
-    
-    public void login(String username, String password){
+
+    public void login(String username, String password) {
         usernameInput.sendKeys(username);
         passwordInput.sendKeys(password);
+
+        /* remember current HTML element for check of staleness. When this element goes stale, the page will
+         * have been reloaded.
+         */
+        WebElement htmlElement = Driver.webDriver.findElement(By.tagName("html"));
 
         submitButton.click();
 
         long startingTime = System.currentTimeMillis();
-        new WebDriverWait(Driver.webDriver, 10).until(ExpectedConditions.stalenessOf(htmlElement));
+        new WebDriverWait(Driver.webDriver, 10)
+                .ignoring(TimeoutException.class)
+                .until(ExpectedConditions.stalenessOf(htmlElement));
 
-        Gauge.writeMessage(String.format("waited %,dms for login", System.currentTimeMillis() - startingTime) );
+        Gauge.writeMessage(String.format("waited %,dms for login", System.currentTimeMillis() - startingTime));
     }
 
-    
+
 }
